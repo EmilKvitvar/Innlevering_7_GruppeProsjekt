5d5c75c261d227c7f808d437e48b4428f61c1504
Emil <emil.kvitvar@gmail.com>
2025-09-23 14:15:18 +0200

    update

--- FunksjonerE.py
+++ FunksjonerE.py
@@ -11,7 +11,7 @@ def check_float(variabel):
                 return float(verdi)
         
         except ValueError:
-            print("Skriv et gyldig tall")
+            print("\nSkriv et gyldig tall")
         
 def check_int(variabel):
     while True:
@@ -27,7 +27,7 @@ def check_int(variabel):
             else:
                 return int(verdi)
         except ValueError:
-            print("Skriv et gyldig tall")
+            print("\nSkriv et gyldig tall")
 
 def check_var(variabel):
     while True:
@@ -37,3 +37,14 @@ def check_var(variabel):
             return None
         else:
             return verdi
+
+def meny():
+    print(f"-----------Meny-----------\
+            \n1. Lag et nytt emne\
+            \n2. Legg til et emne i studieplanen\
+            \n3. Skriv ut ei liste over alle registrerte emner\
+            \n4. Skriv ut studieplanen med hvilke emner som er i hver semester\
+            \n5. Sjekk om studieplanen er gyldig eller ikke\
+            \n6. Lagre emnene og studieplanen til fil\
+            \n7. Les inn emnene og studeplanen fra fil\
+            \nX. Avslutt")
--- Main.py
+++ Main.py
@@ -1,15 +1,19 @@
+import FunksjonerE as E
+
+Emnekoder = []
 
 
 
 while True:
-    Valg = 0
-    print(f"-----------Meny-----------\
-          \n1. Lag et nytt emne\
-          \n2. Legg til et emne i studieplanen\
-          \n3. Skriv ut ei liste over alle registrerte emner\
-          \n4. Skriv ut studieplanen med hvilke emner som er i hver semester\
-          \n5. Sjekk om studieplanen er gyldig eller ikke\
-          \n6. Lagre emnene og studieplanen til fil\
-          \n7. Les inn emnene og studeplanen fra fil")
-    valg = input("["), print(" ]")
-    break
+      Valg = 0
+      #MENY:
+      valg = E.check_int("Valg: ")
+      
+      if valg == None:
+            break
+
+
+
+      else:
+            print("\n Velg et av valgene ")
+            input("Trykk enter")            
